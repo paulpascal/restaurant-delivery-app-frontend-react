d2cce1fd55af88e3c92943c9d4ce064a98c3789c
frivolta <rivoltafilippo@gmail.com>
2020-12-19 19:17:08 +0100

    Adding login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,63 @@
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
+
 export const Login = () => {
+  const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
+
+  const onSubmit = () => {
+    console.log("submittedf");
+  };
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+      <div className="bg-white w-full max-w-lg pt-5 pb-7 rounded-lg text-center">
         <h3 className="text-2xl text-gray-800">Log in</h3>
-        <form className="flex flex-col mt-5 px-5 ">
+        <form
+          className="grid gap-3 mt-5 px-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
-            className="bg-gray-100 shadow-inner mb-3 py-3 px-5 rounded-lg focus:outline-none focus:border-green-600 border-2 focus:border-opacity-50"
+            ref={register({
+              required: "Email is required",
+              pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
+            name="email"
+            required
             type="email"
             placeholder="Email"
+            className="input"
           />
+
+          {errors.email?.type === "pattern" && (
+            <span className="font-medium text-red-500">
+              Please enter a valid email address
+            </span>
+          )}
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email?.message}
+            </span>
+          )}
           <input
-            className="bg-gray-100 shadow-inner mb-3 py-3 px-5 rounded-lg focus:outline-none focus:border-green-600 border-2 focus:border-opacity-50"
+            ref={register({ required: "Password is required" })}
+            required
+            name="password"
             type="password"
             placeholder="Password"
+            className="input"
           />
-          <button className="py-3 px-5 bg-gray-800 text-white text-lg mt-3 rounded-lg focus:outline-none hover:opacity-90">
-            Login
-          </button>
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password?.message}
+            </span>
+          )}
+          <button className="mt-3 btn">Login</button>
         </form>
       </div>
     </div>
